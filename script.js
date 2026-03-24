9bf8f2fd0486eb537ca154fa80354d6f1c3bfcd8
KevinLeon <kevintipto@gmail.com>
2026-03-23 21:36:19 -0500

    feat: variables, tipos de datos e interaccion con usuario

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hola desde JavaScript");
+console.log('¡Hola desde JavaScript!');
+let nombre= "Kevin Lindsey";
+let edad = 27;
+let estaAprendiendo = true; 
+console.log(nombre);
+console.log(edad);
+console.log(estaAprendiendo);
+
+console.log(typeof nombre);
+console.log(typeof edad);
+console.log(typeof estaAprendiendo);
+
+const curso = "Code 101";
+const maxIntentos = 10; 
+
+console.log(curso);
+console.log(maxIntentos);
+
+//maxIntentos=20;
+
+//entrada usuario//
+let nombreUsuario = prompt("Cómo te llamas?");
+let edadUsuario = prompt("Cuántos años tienes?");
+
+//salida usuario//
+alert("hola "+ nombreUsuario+ " tienes " + edadUsuario+ " años");
+
+//verificacion en consola 
+console.log("Nombre: "+ nombreUsuario);
+console.log("Nombre: "+ edadUsuario);
+
+console.log(typeof nombreUsuario);
+console.log(typeof edadUsuario);
+
