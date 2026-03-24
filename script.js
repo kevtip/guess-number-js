451c0a3f7abade04956ddc63068c890f10d97a39
KevinLeon <kevintipto@gmail.com>
2026-03-23 22:05:58 -0500

    feat: operadores aritméticos, concatenación y template literals

--- script.js
+++ script.js
@@ -32,3 +32,36 @@ console.log("Nombre: "+ edadUsuario);
 console.log(typeof nombreUsuario);
 console.log(typeof edadUsuario);
 
+// --- Operadores aritméticos ---
+let a = 20;
+let b = 7;
+
+console.log('Suma:', a + b);        // 27
+console.log('Resta:', a - b);       // 13
+console.log('Multiplicación:', a * b);        
+console.log('División:', a / b);      
+console.log('Módulo:', a % b);        
+
+// que resultado da
+console.log('5' + 3);    // ¿53 o 8?
+console.log(5 + 3);      // ¿53 o 8?
+console.log('5' - 3);    // ¿Qué pasa aquí? el - no concatena como el +, el - obliga a convertirse en nro
+
+// Forma 1: Concatenación con +
+let saludo1 = 'Hola ' + nombreUsuario + ', tienes ' + edadUsuario + ' años.';
+console.log(saludo1);
+
+// Forma 2: Template literals con `` (backticks)
+let saludo2 = `Hola ${nombreUsuario} tienes ${edadUsuario} años`;
+console.log(saludo2);
+
+//calculadora de edad 
+let anioActual =2026;
+let anioNacimiento = anioActual-Number(edadUsuario);
+console.log(`Naciste en el año ${anioNacimiento}`);
+
+let ciudad = prompt("De qué ciudad eres?");
+let mensaje = `Hola ${nombreUsuario} tienes ${edadUsuario} años y eres de la ciudad de ${ciudad}`;
+console.log(mensaje);
+
+
